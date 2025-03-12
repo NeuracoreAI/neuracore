0eb6c8319b29f97b379ec2d19e2851b4e1b7dcc9
stepjam <stepjamuk@gmail.com>
2025-03-12 14:31:39 +0000

    Fix 3.8 -> 3.9

--- setup.py
+++ setup.py
@@ -27,10 +27,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
         "websockets>=14.0",
         "numpy>=1.24.0",
