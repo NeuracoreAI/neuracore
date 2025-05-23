b802c01647a1f650fc13d380fd54f89190df3c1f
stepjam <stepjamuk@gmail.com>
2025-05-23 16:06:51 +0100

    Add additional lossy upload stream

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,7 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
-- None
+- Additional lossy upload stream for faster viewing in the dataset viewer
 
 ### Changed
 
--- neuracore/core/streaming/data_stream.py
+++ neuracore/core/streaming/data_stream.py
@@ -1,7 +1,7 @@
 import logging
 import threading
 from abc import ABC
-from typing import Any
+from typing import Any, List
 
 import numpy as np
 
@@ -41,12 +41,13 @@ class DataStream(ABC):
         self._recording = True
         self._recording_id = recording_id
 
-    def stop_recording(self) -> threading.Thread:
+    def stop_recording(self) -> List[threading.Thread]:
         """Stop recording data."""
         if not self.is_recording():
             raise ValueError("Not recording")
         self._recording = False
         self._recording_id = None
+        return []
 
     def is_recording(self) -> bool:
         """Check if recording is active."""
@@ -72,12 +73,12 @@ class JsonDataStream(DataStream):
         super().start_recording(recording_id)
         self._streamer = StreamingJsonUploader(recording_id, self.filepath)
 
-    def stop_recording(self) -> threading.Thread:
+    def stop_recording(self) -> List[threading.Thread]:
         """Stop video recording and finalize encoding."""
         super().stop_recording()
         upload_thread = self._streamer.finish()
         self._streamer = None
-        return upload_thread
+        return [upload_thread]
 
     def log(self, data: NCData):
         """Convert depth to RGB and log as a video frame."""
@@ -95,25 +96,33 @@ class VideoDataStream(DataStream):
         self.camera_id = camera_id
         self.width = width
         self.height = height
-        self._encoder: VideoDataStream | None = None
+        self._lossless_encoder: VideoDataStream | None = None
+        self._lossy_encoder: VideoDataStream | None = None
 
     def start_recording(self, recording_id: str):
         """Start video recording."""
         super().start_recording(recording_id)
 
-    def stop_recording(self) -> threading.Thread:
+    def stop_recording(self) -> List[threading.Thread]:
         """Stop video recording and finalize encoding."""
         super().stop_recording()
-        upload_thread = self._encoder.finish()
-        self._encoder = None
-        return upload_thread
+        lossless_upload_thread = self._lossless_encoder.finish()
+        lossy_upload_thread = self._lossy_encoder.finish()
+        self._lossless_encoder = None
+        self._lossy_encoder = None
+        return [lossless_upload_thread, lossy_upload_thread]
 
     def log(self, data: np.ndarray, metadata: CameraData):
         """Convert depth to RGB and log as a video frame."""
         self._latest_data = data
-        if not self.is_recording() or self._encoder is None:
+        if (
+            not self.is_recording()
+            or self._lossless_encoder is None
+            or self._lossy_encoder is None
+        ):
             return
-        self._encoder.add_frame(data, metadata)
+        self._lossless_encoder.add_frame(data, metadata)
+        self._lossy_encoder.add_frame(data, metadata)
 
 
 class DepthDataStream(VideoDataStream):
@@ -122,12 +131,23 @@ class DepthDataStream(VideoDataStream):
     def start_recording(self, recording_id: str):
         """Start video recording."""
         super().start_recording(recording_id)
-        self._encoder = StreamingVideoUploader(
+        self._lossless_encoder = StreamingVideoUploader(
             recording_id,
             f"depths/{self.camera_id}",
             self.width,
             self.height,
             depth_to_rgb,
+            codec_context_options={"qp": "0", "preset": "ultrafast"},
+        )
+        self._lossy_encoder = StreamingVideoUploader(
+            recording_id,
+            f"depths/{self.camera_id}/lossy",
+            self.width,
+            self.height,
+            depth_to_rgb,
+            video_name="lossy.mp4",
+            pixel_format="yuv420p",
+            codec="libx264",
         )
 
 
@@ -137,6 +157,19 @@ class RGBDataStream(VideoDataStream):
     def start_recording(self, recording_id: str):
         """Start video recording."""
         super().start_recording(recording_id)
-        self._encoder = StreamingVideoUploader(
-            recording_id, f"rgbs/{self.camera_id}", self.width, self.height
+        self._lossless_encoder = StreamingVideoUploader(
+            recording_id=recording_id,
+            path=f"rgbs/{self.camera_id}",
+            width=self.width,
+            height=self.height,
+            codec_context_options={"qp": "0", "preset": "ultrafast"},
+        )
+        self._lossy_encoder = StreamingVideoUploader(
+            recording_id=recording_id,
+            path=f"rgbs/{self.camera_id}",
+            width=self.width,
+            height=self.height,
+            video_name="lossy.mp4",
+            pixel_format="yuv420p",
+            codec="libx264",
         )
