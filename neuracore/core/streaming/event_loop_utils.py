5921788667b516e274f488b0ec4bbda426901435
Cougar Tasker <cougartasker@gmail.com>
2025-07-24 15:30:27 +0000

    fix: Improved event loop wrap-up

--- neuracore/core/streaming/event_loop_utils.py
+++ neuracore/core/streaming/event_loop_utils.py
@@ -11,6 +11,7 @@ from concurrent.futures import ThreadPoolExecutor
 from typing import Optional
 
 _neuracore_async_loop: Optional[asyncio.AbstractEventLoop] = None
+_loop_lock = threading.Lock()
 
 
 def get_running_loop() -> asyncio.AbstractEventLoop:
@@ -31,19 +32,20 @@ def get_running_loop() -> asyncio.AbstractEventLoop:
         return asyncio.get_running_loop()
     except RuntimeError:
         global _neuracore_async_loop
-        if _neuracore_async_loop is None:
-            _neuracore_async_loop = asyncio.new_event_loop()
-            # Limit the number of threads to a reasonable number
-            executor = ThreadPoolExecutor(
-                max_workers=2, thread_name_prefix="nc-async-executor"
-            )
-            _neuracore_async_loop.set_default_executor(executor)
-            threading.Thread(
-                target=lambda: _neuracore_async_loop.run_forever(),
-                name="nc-async-loop",
-                daemon=True,
-            ).start()
+        with _loop_lock:
+            if _neuracore_async_loop is None:
+                _neuracore_async_loop = asyncio.new_event_loop()
+                # Limit the number of threads to a reasonable number
+                executor = ThreadPoolExecutor(
+                    max_workers=2, thread_name_prefix="nc-async-executor"
+                )
+                _neuracore_async_loop.set_default_executor(executor)
+                threading.Thread(
+                    target=lambda: _neuracore_async_loop.run_forever(),
+                    name="nc-async-loop",
+                    daemon=True,
+                ).start()
 
-        asyncio.set_event_loop(_neuracore_async_loop)
+            asyncio.set_event_loop(_neuracore_async_loop)
 
-        return _neuracore_async_loop
+            return _neuracore_async_loop
